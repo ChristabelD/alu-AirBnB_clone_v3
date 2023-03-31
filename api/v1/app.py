90f55eb5eb4db4946b7e9ec34cf7c516d2e8b892
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-03-31 14:14:41 +0200

    6. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module"""
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def tear_down(self):
     """calls storage.close()"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """error handler for page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":

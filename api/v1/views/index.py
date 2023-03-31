e951d9055bf12046ba5ee811555922553132734e
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-03-31 13:45:03 +0200

    5. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """module"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -10,3 +17,16 @@ def status():
     returns a JSON: "status": "OK"
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats')
+def stats():
+    """retrieves the number of each objects by type"""
+    return jsonify({
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    })

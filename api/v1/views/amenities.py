b0dc483eca2214d6ac120b840c12c0b8475c7264
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-04-01 14:32:21 +0200

    10. User

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,8 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 from flask import make_response, abort, request
-from models.city import City
-from models.state import State
 from models.amenity import Amenity
 
 
